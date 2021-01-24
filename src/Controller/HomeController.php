81d11dd80dcbd57ed15783c9919481bf69c29fa8
IBEGHOUCHENE <ibeghouchene.nadir@gmail.com>
2021-01-24 15:48:43 +0100

    implementation of Doctrine with our first entities

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\Product;
+use App\Repository\ProductRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -12,8 +15,15 @@ class HomeController extends AbstractController
 
     /**
      * @Route("/", name="homepage")
+     * @param EntityManagerInterface $em
+     * @return \Symfony\Component\HttpFoundation\Response
      */
-    public function homepage(){
+    public function homepage( EntityManagerInterface $em){
+
+        $repository = $em->getRepository(Product::class);
+        $product = $repository->find(1);
+        $product->setPrice(5000);
+        $em->flush();
         return $this->render('home.html.twig');
     }
 }
---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Category;
+use App\Entity\Product;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
+use Symfony\Component\String\Slugger\SluggerInterface;
+
+class AppFixtures extends Fixture
+{
+    protected $slogger;
+
+    /**
+     * AppFixtures constructor.
+     * @param SluggerInterface $slogger
+     */
+    public function __construct(SluggerInterface $slogger)
+    {
+        $this->slogger = $slogger;
+    }
+
+    public function load(ObjectManager $manager)
+    {
+        $faker = Factory::create('fr_FR');
+        // ajouter une méthode
+        $faker->addProvider(new \Liior\Faker\Prices($faker));
+        // ne marche pas
+        $faker->addProvider(new \Bezhanov\Faker\Provider\Commerce($faker));
+
+
+        // creer des categorées  : 3
+
+        for ($c = 0; $c < 3; $c++) {
+            $category = new Category;
+            $category->setName($faker->department)  // ca donne une category
+            ->setSlug(strtolower($this->slogger->slug($category->getName())));
+            $manager->persist($category);
+
+            for ($p = 0; $p < mt_rand(15, 20); $p++) {
+                $product = new Product;
+                $product->setName($faker->productName)
+                    ->setPrice($faker->price(4000, 20000))
+                    ->setSlug(strtolower($this->slogger->slug($product->getName())))
+                    ->setCategory($category);
+                $manager->persist($product);
+            }
+            $manager->flush();
+        }
+    }
+     // resume : creer 3 category aleatoire et pour chaque category cerer entre 15 et 20 produits
+}

b8cda902aff6b113d5fb34b1d9e4057725aa2d0c
IBEGHOUCHENE <ibeghouchene.nadir@gmail.com>
2021-01-31 13:20:44 +0100

    Establishment of category validation

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -26,7 +26,7 @@ class CategoryController extends AbstractController
         $category = new Category();
         $form = $this->createForm(CategoryType::class, $category);
         $form->handleRequest($request);
-        if ($form->isSubmitted()) {
+        if ($form->isSubmitted() && $form->isValid()) {
             $category->setSlug(strtolower($slugger->slug($category->getName())));
             $em->persist($category);
             $em->flush();
@@ -52,7 +52,7 @@ class CategoryController extends AbstractController
         $form = $this->createForm(CategoryType::class, $category);
         $form->handleRequest($request);
 
-        if ($form->isSubmitted()) {
+        if ($form->isSubmitted() && $form->isValid()) {
             $category->setSlug($slugger->slug(strtolower($category->getName())));
             $em->flush();
            return $this->redirectToRoute('homepage');
--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -12,8 +12,6 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\String\Slugger\SluggerInterface;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Validator\Constraints\GreaterThan;
-use Symfony\Component\Validator\Constraints\LessThanOrEqual;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 class ProductController extends AbstractController

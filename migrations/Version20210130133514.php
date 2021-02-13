fa2f52b649e0e45d642c7de0d4b663293e2f9545
IBEGHOUCHENE <ibeghouchene.nadir@gmail.com>
2021-02-13 09:39:19 +0100

    Update script migration

--- migrations/Version20210130133514.php
+++ migrations/Version20210130133514.php
@@ -20,12 +20,12 @@ final class Version20210130133514 extends AbstractMigration
     public function up(Schema $schema) : void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE product ADD price DOUBLE PRECISION NOT NULL');
+        // $this->addSql('ALTER TABLE product ADD price DOUBLE PRECISION NOT NULL');
     }
 
     public function down(Schema $schema) : void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE product DROP price');
+        // $this->addSql('ALTER TABLE product DROP price');
     }
 }

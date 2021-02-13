98258081392662ba6ca20635d260df1145119c4f
IBEGHOUCHENE <ibeghouchene.nadir@gmail.com>
2021-02-13 09:31:28 +0100

    delete table entity nadiro

--- migrations/Version20210130133255.php
+++ migrations/Version20210130133255.php
@@ -20,14 +20,14 @@ final class Version20210130133255 extends AbstractMigration
     public function up(Schema $schema) : void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE nadiro (id INT AUTO_INCREMENT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        // $this->addSql('CREATE TABLE nadiro (id INT AUTO_INCREMENT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('ALTER TABLE product DROP price');
     }
 
     public function down(Schema $schema) : void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('DROP TABLE nadiro');
+        // $this->addSql('DROP TABLE nadiro');
         $this->addSql('ALTER TABLE product ADD price INT NOT NULL');
     }
 }

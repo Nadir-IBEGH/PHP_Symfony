bdd3d3d9882245a2fc91c4b9f2a5a882cd0d5a22
IBEGHOUCHENE <ibeghouchene.nadir@gmail.com>
2021-02-14 11:13:35 +0100

    Add Register form

---
+++ src/Controller/RegisterController.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Form\UserType;
+use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+class RegisterController extends AbstractController
+{
+    /**
+     * @Route("/inscription", name="register")
+     * @param Request $request
+     * @param EntityManagerInterface $entityManager
+     * @param UserRepository $userRepository
+     * @param UserPasswordEncoderInterface $encoder
+     * @param EntityManagerInterface $manager
+     * @return Response
+     */
+    public function index(Request $request,
+                          EntityManagerInterface $entityManager,
+                          UserRepository $userRepository,
+                          UserPasswordEncoderInterface $encoder,
+                          EntityManagerInterface $manager
+
+    ): Response
+    {
+        $userForm = new User();
+        $form = $this->createForm(UserType::class, $userForm);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            /** @var User $user */
+            $user = $form->getData();
+            $userFinded = $userRepository->findOneBy(['email' => $user->getEmail()]);
+
+            if ($userFinded) {
+                $this->addFlash('danger', 'Votre adresse email existe déja');
+                return $this->render('register/index.html.twig', [
+                    'formView' => $form->createView()
+                ]);
+            } else {
+
+                $u = new User();
+                $hash = $encoder->encodePassword($u, $user->getPassword());
+                $u
+                    ->setFullName($user->getFullName())
+                    ->setPassword($hash)
+                    ->setEmail($user->getEmail());
+
+                $manager->persist($u);
+                $manager->flush();
+                $this->addFlash('success', 'Félicitation, vous êtes inscrit chez nous, vous pouvez désormé profiter de nos produits');
+                return $this->redirectToRoute('authentification_login');
+            }
+        }
+
+
+        return $this->render('register/index.html.twig', [
+            'formView' => $form->createView()
+        ]);
+    }
+}
---
+++ src/Form/UserType.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class UserType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('fullName', TextType::class,
+                [
+                    'label' => 'Nom complet (nom, prénom)',
+                    'attr' => ['placeholder' => 'Tapez votre nom, prénom']
+                ])
+            ->add('email', RepeatedType::class,
+                [
+                    'type' => EmailType::class,
+                    'invalid_message' => 'L\'email et la confirmation doivet etre identique',
+                    'label' => 'Adresse mail',
+                    'required' => true,
+                    'first_options' => ['label' => 'Adresse mail'],
+                    'second_options' => ['label' => 'Confirmer votre adresse mail']
+                ])
+            ->add('password', RepeatedType::class,
+                [
+                    'type' => PasswordType::class,
+                    'invalid_message' => 'Le mot de passe et la confirmation doivet etre identique',
+                    'label' => 'Mot de passe',
+                    'required' => true,
+                    'first_options' => ['label' => 'Mot de passe'],
+                    'second_options' => ['label' => 'Confirmer votre mot de passe']
+                ]);
+
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
